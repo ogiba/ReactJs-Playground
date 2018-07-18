81932a2ce687ac0a940d4774df8efa278562c6a9
Robert Ogiba <robert.ogiba@outlook.com>
2018-07-19 00:08:09 +0200

    Add simple reacting on onChange event at input element

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Employee from './models/Employee';
 import './App.css';
 
 class App extends Component {
+
+  handleChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
-    let employee = new Employee("bob", "test");
+    let employee = new Employee("bobo", "test");
 
     return (
       <div className="App">
@@ -15,6 +20,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+
+          <input onChange={this.handleChange}/>
+          <div>{employee.lastName}</div>
         </p>
       </div>
     );
